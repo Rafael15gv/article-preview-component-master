32259b4fd6ba6e78ac2bee15bbfba5fb909d5959
Rafael15gv <143367099+Rafael15gv@users.noreply.github.com.>
2025-03-07 09:34:24 -0500

    fix js and CSS, pendiente arreglar un detalle con el icono share.

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const btn = document.getElementsByClassName("profile__btn")[0];
 btn.addEventListener("click", function () {
   // alert("hola");
   document.querySelector(".redes__black").classList.remove("hiden");
-  document.querySelector(".profile").classList.add("hiden");
+  document.querySelector(".triangulo").classList.remove("hiden");
+  document.querySelector(".profile__img").classList.add("hiden");
+  document.querySelector(".profile__p").classList.add("hiden");
 });
